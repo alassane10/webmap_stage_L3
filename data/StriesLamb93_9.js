a0f4769f9b552ff45edceeef25387cb45d4cd694
alassane10 <105866727+alassane10@users.noreply.github.com>
2022-05-24 18:14:36 +0200

    webmap

---
+++ data/StriesLamb93_9.js
@@ -0,0 +1,10 @@
+var json_StriesLamb93_9 = {
+"type": "FeatureCollection",
+"name": "StriesLamb93_9",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": 1.0, "descriptio": "Stries", "Formations": "F12", "Dip": "40", "Dip direct": "128", "Stratifica": "S", "Longitude": 6.28221, "Latitude": 44.21222 }, "geometry": { "type": "Point", "coordinates": [ 6.282209, 44.212221 ] } },
+{ "type": "Feature", "properties": { "fid": 2.0, "descriptio": "Stries", "Formations": "F12", "Dip": "12", "Dip direct": "188", "Stratifica": "S", "Longitude": 6.28224, "Latitude": 44.21222 }, "geometry": { "type": "Point", "coordinates": [ 6.282239, 44.212215 ] } },
+{ "type": "Feature", "properties": { "fid": 3.0, "descriptio": "Stries", "Formations": "F9", "Dip": "10", "Dip direct": "274", "Stratifica": "S", "Longitude": null, "Latitude": null }, "geometry": { "type": "Point", "coordinates": [ 6.257189261701943, 44.235382137010987 ] } }
+]
+}

98e439166b788496e1e466744040c9563ab0290b
alassane10 <105866727+alassane10@users.noreply.github.com>
2022-05-24 17:10:46 +0200

    webmap

---
+++ data/Failles1_70.js
@@ -0,0 +1,13 @@
+var json_Failles1_70 = {
+"type": "FeatureCollection",
+"name": "Failles1_70",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": 2.0, "descriptio": "45° / 181°\nUnit 2 (Hettangien)\nFault\n\n\nDeclination: -0.03°", "Formations": "F2-6", "Dip": "45", "Dip direct": "181", "Longitude": 6.26614, "Latitude": 44.26217 }, "geometry": { "type": "Point", "coordinates": [ 6.266143, 44.262166 ] } },
+{ "type": "Feature", "properties": { "fid": 3.0, "descriptio": "58° / 207°\nUnit 2 (Hettangien)\nFault\n\n\nDeclination: -0.03°", "Formations": "F2-6", "Dip": "58", "Dip direct": "207", "Longitude": 6.26614, "Latitude": 44.26217 }, "geometry": { "type": "Point", "coordinates": [ 6.266143, 44.262166 ] } },
+{ "type": "Feature", "properties": { "fid": 4.0, "descriptio": "28° / 122°\nFormation 12\nFault\n\n\nDeclination: 0.25°", "Formations": "F12", "Dip": "28", "Dip direct": "122", "Longitude": 6.2822, "Latitude": 44.21225 }, "geometry": { "type": "Point", "coordinates": [ 6.282199, 44.21225 ] } },
+{ "type": "Feature", "properties": { "fid": 5.0, "descriptio": "Fault\n", "Formations": "F8", "Dip": "38", "Dip direct": "15", "Longitude": 6.2769, "Latitude": 44.24063 }, "geometry": { "type": "Point", "coordinates": [ 6.276895224697161, 44.240627415258515 ] } },
+{ "type": "Feature", "properties": { "fid": 6.0, "descriptio": "Fault\n", "Formations": "F3", "Dip": "88", "Dip direct": "58", "Longitude": 6.23538, "Latitude": 44.25239 }, "geometry": { "type": "Point", "coordinates": [ 6.235378860257021, 44.252394934893708 ] } },
+{ "type": "Feature", "properties": { "fid": 7.0, "descriptio": "Fault\n", "Formations": "F6", "Dip": "58", "Dip direct": "238", "Longitude": 6.24308, "Latitude": 44.2633 }, "geometry": { "type": "Point", "coordinates": [ 6.243078417834955, 44.263298550349404 ] } }
+]
+}
---
+++ data/Failles2_69.js
@@ -0,0 +1,23 @@
+var json_Failles2_69 = {
+"type": "FeatureCollection",
+"name": "Failles2_69",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "2", "Formations": "F2-6", "Natures": "Failles" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 6.241328518385437, 44.264379402065153 ], [ 6.241765993247817, 44.259350923088846 ] ] ] } },
+{ "type": "Feature", "properties": { "id": "1", "Formations": "F2-6", "Natures": "Failles" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 6.239316134018477, 44.262155018929484 ], [ 6.240847296036815, 44.261920043965084 ] ] ] } },
+{ "type": "Feature", "properties": { "id": "3", "Formations": "F2-6", "Natures": "Failles" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 6.239009901614806, 44.261826053716369 ], [ 6.23995047256893, 44.260839147032684 ] ] ] } },
+{ "type": "Feature", "properties": { "id": "4", "Formations": "F2-6", "Natures": "Failles" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 6.238419310550588, 44.261528416937459 ], [ 6.239556745192784, 44.260447512804959 ] ] ] } },
+{ "type": "Feature", "properties": { "id": "5", "Formations": "F2-6", "Natures": "Failles" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 6.237981835688206, 44.261074126317162 ], [ 6.238791164183615, 44.260337854753708 ] ] ] } },
+{ "type": "Feature", "properties": { "id": "6", "Formations": "F2-6", "Natures": "Failles" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 6.237522487082705, 44.260541505257542 ], [ 6.238222446862518, 44.259711233394469 ] ] ] } },
+{ "type": "Feature", "properties": { "id": "7", "Formations": "F2-6", "Natures": "Failles" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 6.236931896018491, 44.260134203544482 ], [ 6.238178699376281, 44.259758230228073 ] ] ] } },
+{ "type": "Feature", "properties": { "id": "8", "Formations": "F2-6", "Natures": "Failles" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 6.236494421156109, 44.259726899009848 ], [ 6.236953769761611, 44.258630295853578 ] ] ] } },
+{ "type": "Feature", "properties": { "id": "9", "Formations": "F2-6", "Natures": "Failles" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 6.236603789871703, 44.258614629946024 ], [ 6.235597597688222, 44.259194265745535 ] ] ] } },
+{ "type": "Feature", "properties": { "id": "10", "Formations": "F2-6", "Natures": "Failles" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 6.236603789871701, 44.25863029585355 ], [ 6.234810142935935, 44.25886528396596 ] ] ] } },
+{ "type": "Feature", "properties": { "id": "11", "Formations": "F2-6", "Natures": "Failles" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 6.234285173101076, 44.258473636590232 ], [ 6.236560042385465, 44.25820731488465 ] ] ] } },
+{ "type": "Feature", "properties": { "id": "12", "Formations": "F2-6", "Natures": "Failles" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 6.233913319468049, 44.258128984741646 ], [ 6.235072627853364, 44.256954020075113 ] ] ] } },
+{ "type": "Feature", "properties": { "id": "13", "Formations": "F14-F2.6", "Natures": "Chevauchement" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 6.244497288972977, 44.201238774604882 ], [ 6.245905939854799, 44.200228910423547 ], [ 6.246826980815992, 44.199879338020182 ], [ 6.247422948496763, 44.199685130233249 ], [ 6.248073095057604, 44.1993743964426 ], [ 6.248723241618446, 44.199063661013192 ], [ 6.249427567059357, 44.198908292683939 ] ] ] } },
+{ "type": "Feature", "properties": { "id": null, "Formations": "F8", "Natures": "Failles" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 6.276847537072698, 44.240293831243022 ], [ 6.274220100972331, 44.240219527979889 ] ] ] } },
+{ "type": "Feature", "properties": { "id": null, "Formations": "F8", "Natures": "Failles" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 6.272871811394508, 44.240368134412336 ], [ 6.271385235706142, 44.240516740469424 ] ] ] } },
+{ "type": "Feature", "properties": { "id": null, "Formations": "F8", "Natures": "Failles" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 6.265957505867219, 44.241160695712807 ], [ 6.264125214902489, 44.243464016389183 ] ] ] } }
+]
+}
